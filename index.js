ebdc3db472a9fec0480ce2bed6064361658c06ba
yonle <fledyonle@gmail.com>
2021-04-07 21:10:52 +0800

    Small changes

--- index.js
+++ index.js
@@ -5,23 +5,28 @@ const throttle = require("throttle");
 const ffmpeg = require("prism-media").FFmpeg;
 const ytdl = require("ytdl-core");
 
+// null = Random
+// Max: 18
+// Default: null / 3 is recommended
+var random_query_length = null;
+
 function convert() {
-        return new ffmpeg({
-                args: args: ["-analyzeduration", "0", "-loglevel", "0", "-f", "mp3", "-ar", "48000", "-ac", "2", "-ab", "192", "-map", "0:a", "-map_metadata", "-1"]
-        });
+  return new ffmpeg({
+    args: ["-analyzeduration", "0", "-loglevel", "0", "-f", "mp3", "-ar", "48000", "-ac", "2", "-ab", "192", "-map", "0:a", "-map_metadata", "-1"]
+  });
 };
 
 var url = fs.readFileSync("yturl.txt", 'utf8');
 
 // Query
 var curSong = {
-	name: null,
-	id: null
+  name: null,
+  id: null
 };
 
 var nextSong = {
-	name: null,
-	id: null
+  name: null,
+  id: null
 }
 // Sink management
 var sink = new Map();
@@ -29,87 +34,89 @@ var wsClient = new Map();
 
 // Server
 
-var server = http.createServer(function (req, res) {
-	var id = Math.random().toString(36).slice(2);
-	res.setHeader("content-type", "audio/mpeg");
-	res.setHeader("title", curSong.title || "No songs....");
-	sink.set(id, res);
+var server = http.createServer(function(req, res) {
+  var id = Math.random().toString(36).slice(2);
+  res.setHeader("content-type", "audio/mpeg");
+  res.setHeader("title", curSong.title || "No songs....");
+  sink.set(id, res);
 
-	req.on('close', function () {
-		sink.delete(id);
-	});
+  req.on('close', function() {
+    sink.delete(id);
+  });
 });
 
 server.listen(8080, () => launch());
 server.on('error', console.error);
 
-server.broadcast = (function (data) {
-	sink.forEach(function (res, id) {
-		res.write(data, error => {
-			if (error) {
-				sink.delete(id);
-			}
-		});
-	});
+server.broadcast = (function(data) {
+  sink.forEach(function(res, id) {
+    res.write(data, error => {
+      if (error) {
+        sink.delete(id);
+      }
+    });
+  });
 });
 
 // Websocket, for Song name information
 var wss = new WebSocket({ server });
 
 wss.on('connection', (ws, req) => {
-	var id = Math.random().toString(36).slice(2);
-	wsClient.set(id, ws);
-	if (curSong.name != null) ws.send(curSong.name);
-	req.on('close', function () {
-		wsClient.delete(id);
-	});
+  var id = Math.random().toString(36).slice(2);
+  wsClient.set(id, ws);
+  if (curSong.name != null) ws.send(curSong.name);
+  req.on('close', function() {
+    wsClient.delete(id);
+  });
 });
 
-wss.broadcast = (function (data) {
-	wsClient.forEach(function (ws, id) {
-		ws.send(data, function (error) {
-			if (error) {
-				wsClient.delete(id);
-			}
-		});
-	});
+wss.broadcast = (function(data) {
+  wsClient.forEach(function(ws, id) {
+    ws.send(data, function(error) {
+      if (error) {
+        wsClient.delete(id);
+      }
+    });
+  });
 });
 
 // Player
-var launch = function () {
-	if (!url || url.length < 1) {
-		console.error("No youtube URL provided. Aborting....")
-		return process.exit(1);
-	} else { 
-		console.log('Radio is now listening on port', 8080);
-		return play();
-	}
+var launch = function() {
+  if (!url || url.length < 1) {
+    console.error("No youtube URL provided. Aborting....");
+    return process.exit(1);
+  } else {
+    console.log('Radio is now listening on port', 8080);
+    return play();
+  }
 };
 
-var play = function () {
-	if (nextSong.id) {
-		url = `https://youtu.be/${nextSong.id}`;
-		fs.writeFileSync('yturl.txt', url, 'utf8');
-	}
-	var stream = ytdl(url, { filter: 'audioonly', quality: 'highestaudio'});
-	stream.on('info', async function (info) {
-		curSong.name = info.videoDetails.title;
-                curSong.id = info.videoDetails.id;
-		nextSong.id = info.related_videos[0].id;
-		nextSong.name = info.related_videos[0].title;
-		// Convert into mp3
-		audio = stream.pipe(convert());
-		// Then broadcast it
-		broadcast(audio, curSong.name);
-	});
-        stream.on('error', console.error);
+var play = function() {
+  if (nextSong.id) {
+    url = `https://youtu.be/${nextSong.id}`;
+    fs.writeFileSync('yturl.txt', url, 'utf8');
+  }
+  var stream = ytdl(url, { filter: 'audioonly', quality: 'highestaudio' });
+  stream.on('info', async function(info) {
+    var randomQuery = Math.floor(Math.random() * (random_query_length || info.related_videos.length));
+    curSong.name = info.videoDetails.title;
+    curSong.id = info.videoDetails.id;
+    nextSong.id = info.related_videos[randomQuery].id;
+    nextSong.name = info.related_videos[randomQuery].title;
+    // Convert into mp3
+    audio = stream.pipe(convert());
+    // Then broadcast it
+    broadcast(audio, curSong.name);
+  });
+  stream.on('error', console.error);
 };
 
-var broadcast = function (ReadStream, title) {
-	stream = new throttle(24000);
-	ReadStream.pipe(stream);
-	wss.broadcast(title);
-	console.log('-> Now Playing:', title);
-	stream.on('data', server.broadcast);
-	stream.on('end', play);
+// Broadcast Engine
+var broadcast = function(ReadStream, title) {
+  stream = new throttle(24000);
+  ReadStream.pipe(stream);
+  wss.broadcast(title);
+  console.log('-> Now Playing:', title);
+  stream.on('data', server.broadcast);
+  stream.on('end', play);
 };
